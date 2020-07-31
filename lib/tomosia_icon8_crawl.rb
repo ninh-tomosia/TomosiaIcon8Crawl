f56e0867942ccdb470a434cf525c260b942e4548
ninh-tomosia <tt.ninh.le@tomosia.com>
2020-07-31 16:09:24 +0700

    update export excel

--- lib/tomosia_icon8_crawl.rb
+++ lib/tomosia_icon8_crawl.rb
@@ -26,9 +26,53 @@
 	      	end
     	end
 
-    	# def
+    	def save_file_excel(data = {})
+    		begin
+
+    			workbook  = WriteExcel.new('./export.xls')
+    			format = workbook.add_format
+				format.set_bold()
+				format.set_align('center')
+
+				data_col = workbook.add_format
+				data_col.set_align('center')
+				format_url = workbook.add_format
+				format_url.set_color('blue')
+				format_url.set_align('center')
+
+	    		worksheet = workbook.add_worksheet
+
+	    		worksheet.write_string(0, 0, 'STT', format)
+		    	worksheet.write_string(0, 1, 'NAME', format)
+		    	worksheet.write_string(0, 2, 'URL', format)
+		    	worksheet.write_string(0, 3, 'SIZE', format)
+		    	worksheet.write_string(0, 4, 'EXTENSION', format)
+
+		    	data.each_with_index do |row, index|
+		    		i = index + 1
+		    		# p i
+		    		# p row
+		    		row.each do |key, value|
+		    			# p key
+
+			    		worksheet.write_string(i, 0, row['index'], data_col)
+			    		worksheet.write_string(i, 1, row['name'], data_col)
+			    		worksheet.write_url(i, 2, row['url'], format_url)
+			    		worksheet.write_string(i, 3, row['size'], data_col)
+			    		worksheet.write_string(i, 4, row['extension'], data_col)
+		    		end
+		    	end
+
+	    		workbook.close
+    		rescue Exception => e
+    			p "Can't saved file"
+    			p e
+    			# break
+    		end
+    	end
 
         def crawl
+        	data = []
         	html
             i = 0
 			for i in 0..(@icons.to_a.length - 1)
@@ -36,35 +80,41 @@
 					begin
 						# Title image
 						title = @icons.css('.text').to_a[i]
-						p i.to_s + " " + title
+						# p i.to_s + " " + title
 
 						# src image
 						image = @icons.css('img').to_a[i].to_h['src']
-						p image
+						# p image
 
 						# Path download image
 						des = @path. + "/" + i.to_s + "-" + title + ".png"
-						p des
+						# p des
 
 						# Extension image
 						ext = File.extname(image)
-						p ext
+						# p ext
 
 						# Name image
 						img_name = File.basename(image, ext)
 						p img_name
 
 						# download image
-						File.open(des, 'wb') do |file|
+						File.open(des, 'w+') do |file|
 							file.write open(image).read
 						end
 
 						# Size image
-						size = File.size(des)
+						size_n = File.size(des)
+						size = size_n.to_s + 'Kb'
 						p size
 
+						row = {"index" => i, "name" => img_name, "url" => image, "size" => size, "extension" => ext}
+						data.push(row)
+
+						# p data
 						# Save file
-						save_file_txt(i, img_name, image, size , ext)
+						# save_file_txt(i, img_name, image, size , ext)
+						# save_file_excel(data)
 
 					rescue Exception => e
 						p "--Runtime error--"
@@ -73,8 +123,9 @@
 					end
 				end
 	        end
+	        save_file_excel(data)
 	    end
 
     end
 # end
-CrawlIcon8.new("corona", "../img", 200).crawl
+CrawlIcon8.new("corona", "../img", 10).crawl
