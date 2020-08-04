08a2ffb64aabf4acbf80a20c3b250c99041e0c8a
ninh-tomosia <tt.ninh.le@tomosia.com>
2020-08-04 14:09:14 +0700

    update gem crawl icon8

--- lib/tomosia_icon8_crawl.rb
+++ lib/tomosia_icon8_crawl.rb
@@ -5,20 +5,15 @@ module TomosiaIcon8Crawl
 	require 'WriteExcel'
     class CrawlIcon8
 
-    	def initialize(keyword, destination, max)
-    		@key = keyword
-    		@path = destination
-    		@max = max
-    	end
-
-    	def html
-    		if @key == nil
+    	# get data from website
+    	def self.json(key, max)
+    		if key == nil
     			p "No data!"
     		else 
-    			if @max == nil
-    				url = "https://search.icons8.com/api/iconsets/v5/search?term=#{@key}"
+    			if max == nil
+    				url = "https://search.icons8.com/api/iconsets/v5/search?term=#{key}"
     			else
-    				url = "https://search.icons8.com/api/iconsets/v5/search?term=#{@key}&amount=#{@max}"
+    				url = "https://search.icons8.com/api/iconsets/v5/search?term=#{key}&amount=#{max}"
     			end
     		end
     		
@@ -26,16 +21,18 @@ module TomosiaIcon8Crawl
 			@responses = page_data.parsed_response	
     	end
 
-    	def save_file_txt(index, name, url, size, extension)   
+    	# save file to txt
+    	def self.save_file_txt(index, name, url, size, extension)   
 	      	File.open("log_image.txt", "a+") do |f|
 	      		f.write("#{index}. name: #{name} | url: #{url} | size: #{size}Kb | extension: #{extension} \n")
 	      	end
     	end
 
-    	def save_file_excel(data = {})
+    	# save file to excel
+    	def self.save_file_excel( path, data = {})
     		begin
-
-    			workbook  = WriteExcel.new('./export.xls')
+    			des = path + '/export.xls'
+    			workbook  = WriteExcel.new(des)
     			format = workbook.add_format
 				format.set_bold()
 				format.set_align('center')
@@ -77,13 +74,36 @@ module TomosiaIcon8Crawl
     		end
     	end
 
-        def crawl
+    	# download image
+    	def self.download_image(path, img)
+			File.open(path, 'wb') do |file|
+				file.write open(img).read
+			end			
+    	end
+
+    	# multi download image
+    	def self.multi_download_image(path, imgs)
+    		threads = []
+
+    		imgs.each_with_index do |img, index|
+    			# p index
+    			title = File.basename(img, '.png')
+    			des = path. + "/" + index.to_s + "-" + title + ".png"
+				threads << Thread.new{download_image(des, img)}
+				threads.each{ |t| t.join }
+			end
+    	end
+
+    	# main
+        def self.crawl(keyword = nil, path = ".", max = nil)
         	begin
 	        	data = []
-	        	html
+				images = []
+				des = ""
+	        	json(keyword, max)
 				@responses['icons'].each_with_index do |item, index| 
 					title = item['commonName']
-					des = @path. + "/" + index.to_s + "-" + title + ".png"
+					
 
 					src = "https://img.icons8.com/#{item['platform']}/2x/#{item['commonName']}.png"
 					# Extension image
@@ -92,21 +112,20 @@ module TomosiaIcon8Crawl
 					# Name image
 					img_name = File.basename(src, ext)
 
-					# download image
-					File.open(des, 'wb') do |file|
-						file.write open(src).read
-					end
-
+					# add image
+					images.push(src)
+					
 					# Size image
-					size_n = File.size(des)
-					size = size_n.to_s + 'Kb'
+					size = src.size.to_s + 'px'
 
 					# push data
 					row = {"index" => index, "name" => img_name, "url" => src, "size" => size, "extension" => ext}
 					data.push(row)
 				end
-				p data
-		        save_file_excel(data)
+				# p data
+
+		        save_file_excel(path, data)
+				multi_download_image(path, images)
 	    	rescue Exception => e
 	    		p "--Runtime error--"
 	    		p e
@@ -115,3 +134,5 @@ module TomosiaIcon8Crawl
 
     end
 end
+
+# TomosiaIcon8Crawl::CrawlIcon8.crawl("coronavirus")
